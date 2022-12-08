c0f69c640ce14d2cd19682991c9dddfb191fede1
nikitozvyag <mykytazviahin@gmail.com>
2022-12-08 20:58:49 +0200

    fix: lesson-2 fix

--- lesson-2/work-with-json-files/books/index.js
+++ lesson-2/work-with-json-files/books/index.js
@@ -1,9 +1,8 @@
 const fs = require("fs/promises");
-const ObjectID = require("bson-objectid");
-
 const path = require("path");
+const ObjectId = require("bson-objectid");
 
-const booksPath = path.join(__dirname, "./books.json");
+const booksPath = path.join(__dirname, "books.json");
 
 const getAll = async () => {
   const result = await fs.readFile(booksPath);
@@ -12,30 +11,30 @@ const getAll = async () => {
 
 const getByID = async (id) => {
   const books = await getAll();
-  const book = books.find((item) => item.id === id);
-  if (!books) return null;
-  return book;
+  const result = await books.find((item) => item.id === id);
+  if (!result) return null;
+  return result;
 };
 
 const add = async (title, author) => {
+  const books = await getAll();
   const newBook = {
     title,
     author,
-    id: ObjectID(),
+    id: ObjectId(),
   };
-  const books = await getAll();
   books.push(newBook);
   await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
   return newBook;
 };
 
-const removeById = async (id) => {
+const removeByID = async (id) => {
   const books = await getAll();
   const idx = books.findIndex((item) => item.id === id);
   if (idx === -1) return null;
-  const [removedBook] = books.splice(idx, 1);
-  await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
-  return removedBook;
+  const [result] = books.splice(idx, 1);
+	await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
+  return result;
 };
 
-module.exports = { getAll, getByID, add, removeById };
+module.exports = { getAll, getByID, add, removeByID };
