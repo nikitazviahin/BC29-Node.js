e4d18984ea7f7f429f687d4855414a1fc8c9c371
nikitozvyag <mykytazviahin@gmail.com>
2022-12-13 20:42:46 +0200

    feat: add lesson-6

---
+++ lesson-6/mongodb-project/models/book.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+
+const bookSchema = Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    genre: {
+      type: String,
+      enum: ["fiction", "drama"],
+      required: true,
+    },
+    isbn: {
+      type: String,
+      required: true,
+      match: /[0-9]{3}-[0-9]{1}-[0-9]{3}-[0-9]{5}-[0-9]{1}/
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const Book = model("books", bookSchema);
+
+module.exports = Book;
---
+++ lesson-6/readme.md
@@ -0,0 +1,9 @@
+[Схема роботи dotenv](./slides/dotenv-work-schema.jpg)
+
+[Структура MongoDB](./slides/mongodb-database-structure.jpg)
+
+[Схема роботи з MongoDB](./slides/remote-database.jpg)
+
+[Принцип роботи моделі](./slides//schema-model-collection.jpg)
+
+[Схема використання різних моделей](./slides/schema-model-example.jpg)
